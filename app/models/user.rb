128b424b2c80c1d886335828b9f704cfc292385c
Mateo Vidal <jvidalba@gmail.com>
2024-04-23 15:24:04 -0500

    Add constraints for emails

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   has_secure_password
 
   validates_presence_of :first_name, :last_name, :email
+  validates_uniqueness_of :email
+  validates_format_of :email, with: URI::MailTo::EMAIL_REGEXP
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -34,5 +34,17 @@ RSpec.describe User, type: :model do
       subject.birthday = nil
       expect(subject).to be_valid
     end
+
+    it 'is invalid with wrong email format' do
+      subject.email = 'testing_oelo.co'
+      expect(subject).to be_invalid
+    end
+
+    it 'is invalid with an email already taken' do
+      old_user = create(:user)
+      subject.email = old_user.email
+
+      expect(subject).to be_invalid
+    end
   end
 end
