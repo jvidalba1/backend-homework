0190944717ef5cd4cd1c6422c4f05e37aa7374d2
Mateo Vidal <jvidalba@gmail.com>
2024-04-23 21:35:15 -0500

    Add email and password custom validators

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,9 @@
 class User < ApplicationRecord
   has_secure_password
 
-  PASSWORD_REGEX = /\A(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#?\]])(?=.*[\W]).{10,}\z/
+  validates :email, format_email: true
+  validates_uniqueness_of :email
+  validates :password, format_password: true
 
   validates_presence_of :first_name, :last_name, :email
-  validates_uniqueness_of :email
-  validates_format_of :email, with: URI::MailTo::EMAIL_REGEXP
-  validates_format_of :password, with: PASSWORD_REGEX
 end
---
+++ app/validators/format_password_validator.rb
@@ -0,0 +1,11 @@
+class FormatPasswordValidator < ActiveModel::EachValidator
+
+  PASSWORD_REGEX = /\A(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#?\]])(?=.*[\W]).{10,}\z/
+
+  def validate_each(record, attribute, value)
+    unless value =~ PASSWORD_REGEX
+      # record.errors[attribute] << (options[:message] || 'is invalid')
+      record.errors.add(attribute, 'is invalid')
+    end
+  end
+end
