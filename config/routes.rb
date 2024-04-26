4546a0cb7238ce7b3386989bdbe817d6c9589024
Mateo Vidal <jvidalba@gmail.com>
2024-04-26 01:44:12 -0500

    Add namespace according to controller refactor

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   resources :sessions, only: [:create]
   resources :registrations, only: [:create]
+  scope module: 'resources' do
+    resources :movies, only: [:index, :create, :update]
+  end
+
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
