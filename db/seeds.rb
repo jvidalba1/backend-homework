8629969157507d24e27f8e93fb085a507a47dbad
Mateo Vidal <jvidalba@gmail.com>
2024-04-26 01:45:26 -0500

    Add initial values for db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,70 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+user1 = User.create!(
+  first_name: Faker::Name.first_name,
+  last_name: Faker::Name.last_name,
+  password: "TestingUser1!",
+  birthday: Faker::Date.birthday,
+  email: 'testing_user1@sample.com'
+)
+
+user2 = User.create!(
+  first_name: Faker::Name.first_name,
+  last_name: Faker::Name.last_name,
+  password: "TestingUser2!",
+  birthday: Faker::Date.birthday,
+  email: 'testing_user2@sample.com'
+)
+
+# Public movies
+
+3.times do
+  Movie.create!(
+    name: Faker::Movie.title,
+    user: user1,
+    producer: Faker::Name.name,
+    accessibility: 1,
+    released_date: Faker::Date.backward
+  )
+end
+
+3.times do
+  Movie.create!(
+    name: Faker::Movie.title,
+    user: user2,
+    producer: Faker::Name.name,
+    accessibility: 1,
+    released_date: Faker::Date.backward
+  )
+end
+
+# Private movies
+
+3.times do
+  Movie.create!(
+    name: Faker::Movie.title,
+    user: user1,
+    producer: Faker::Name.name,
+    accessibility: 0,
+    released_date: Faker::Date.backward
+  )
+end
+
+3.times do
+  Movie.create!(
+    name: Faker::Movie.title,
+    user: user2,
+    producer: Faker::Name.name,
+    accessibility: 0,
+    released_date: Faker::Date.backward
+  )
+end
+
+# MUST ToDos:
+# - Pagination
+# - Missing tests
+# - Readme
+# - Serialization responses
+
+# NICE TO HAVE ToDos:
+# - Heroku deployment
+# - Documentation (apps?)
+# - Endpoint that requires server to retrieve a random number from a public API and send it back to the user
